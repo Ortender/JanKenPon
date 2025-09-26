c7e8fa017f72eb1f70bba8858518f3942d9ee57a
Ortender <charles.tonneau@gmail.com>
2025-09-26 11:44:38 +0200

    Application complète mais avec un problème d'event listner

---
+++ scripts/main.js
@@ -0,0 +1,34 @@
+/*********************************************************************************
+ * 
+ * Starting point, this file initiate the game. 
+ * 
+ *********************************************************************************/
+
+// We get all the elements that need to either be displayed or be removed during the game
+let btnnewgame = document.getElementById("btnnewgame")
+let vsicon = document.getElementById("vsicon")
+let choicesection = document.querySelector(".userchoicesection")
+let playerhealthbar = document.querySelectorAll(".playerhealth")
+let pchealthbar = document.querySelectorAll(".PChealth")
+let result_image = document.getElementById("resultimage")
+let KOimage = document.querySelectorAll(".KOimage img")
+let choiceicons = document.querySelectorAll(".choiceicon")
+
+// We setup the healthpoint of the characters to "full" by indexing it at 0, it'll help us go through the different images
+let playerhealth = 0
+let PChealth = 0
+
+/**
+ * We setup a listner on the "New game" button
+ * When the user click on "New game" we initiate the game
+ */
+btnnewgame.addEventListener( "click", () => {
+    // we hide some elements if the player was already playing
+    hide_element(playerhealthbar[playerhealth])
+    hide_element(pchealthbar[PChealth])
+    for (i=0; i < KOimage.length; i++) {
+        hide_element(KOimage[i])
+    }
+    // we launch the game
+    launchgame()
+})
---
+++ scripts/script.js
@@ -0,0 +1,163 @@
+/*********************************************************************************
+ * 
+ * This file contains all the functions required for the game to run. 
+ * 
+ *********************************************************************************/
+
+function launchgame() {
+    playerhealth = 0
+    PChealth = 0
+    // We hide the new game button
+    hide_element(btnnewgame)
+    // We display the rest of the elements
+    display_element(vsicon)
+    display_element(playerhealthbar[0])
+    display_element(pchealthbar[0])
+    display_element(choicesection)
+   /*for (i=0; i < choiceicons.length; i++) {
+        console.log(i)
+        display_element(choiceicons[i])
+    }*/
+    // Now we wait for the player to make his play
+    // We monitor when the player chooses an option between rock, paper and scissor by clicking on the corresponding icon
+    for (index=0; index < choiceicons.length; index++) {
+        choiceicons[index].addEventListener( "click", (event) => {
+            // we retrieve the id of the icon that was clicked on as the user choice
+            let user_choice = event.target.id
+            // we change the character image to the corresponding choice
+            // we get the computer random choice
+            let computer_choice = get_computer_choice()
+            console.log(user_choice, computer_choice)
+            change_character_image("Alexx", user_choice)
+            change_character_image("computer", computer_choice)
+            //we now display the result of the this round, first we identify the winner
+            let round_result = identify_winner(user_choice, computer_choice)
+            //then we display the right result text image "draw", "you win", "you lose"
+            display_result_test(result_image, round_result)
+            display_element(result_image)
+            //then based on the result we impact the character health
+            switch (round_result) {
+                case "draw":
+                    break
+                case "win":
+                    PChealth++
+                    healthbar_management(pchealthbar, PChealth)
+                    if (PChealth > 3) {
+                        endgame("computer", playerhealth, PChealth)
+                    }
+                    break
+                case "lose":
+                    playerhealth++
+                    healthbar_management(playerhealthbar, playerhealth)
+                    if (playerhealth > 3) {
+                        endgame("player", playerhealth, PChealth)
+                    }
+                    break
+            }
+        })
+        choiceicons[index].removeEventListener( "click", (event))
+    }
+}
+
+/**
+ * this function removes the "hide" css class from a page element in order to display it.
+ * @param {DOM element} element : the DOM element we want to display
+ */
+function display_element(element) {
+    element.classList.remove("hide")
+}
+
+/**
+ * this function adds the "hide" css class to a page element in order to hide it.
+ * @param {DOM element} element : the DOM element we want to hide
+ */
+function hide_element(element) {
+    element.classList.add("hide")
+}
+
+/**
+ * this function returns a random choice for the computer between 1 and 3.
+ */
+function get_computer_choice() {
+    switch (Math.floor(Math.random() * 3)) {
+        case 0:
+            return "rock"
+        case 1:
+            return "paper"
+        case 2:
+            return "scissors"
+    }
+}
+
+/**
+ * this function identify the winner by comparing the choices provided by the player and the computer.
+ */
+function identify_winner(user_choice, computer_choice) {
+    if (user_choice === computer_choice) {
+        return "draw"
+    } else {
+        switch (user_choice) {
+            case "rock":
+                if (computer_choice === "scissors") {
+                    return "win"
+                } else {
+                    return "lose"
+                }
+            case "paper":
+                if (computer_choice === "rock") {
+                    return "win"
+                } else {
+                    return "lose"
+                }
+            case "scissors":
+                if (computer_choice === "paper") {
+                    return "win"
+                } else {
+                    return "lose"
+                }
+        }
+    }
+}
+
+/**
+ * this function manage the display of the right health bar when the player or computer lose health.
+ * @param {DOM element} healthbar : the image element we want to hide or display
+ * @param {Int} healthpoint : the current status of health of the Character
+ */
+function healthbar_management(healthbar, healthpoint) {
+    healthbar[healthpoint-1].classList.add("hide")
+    healthbar[healthpoint].classList.remove("hide")
+}
+
+
+/**
+ * this function manage the display of the right character image depending on the chosen action.
+ * @param {string} choice : the action to display
+ * @param {string} character : which of the two character we want to change the image PC or Player
+ */
+function change_character_image(character, choice) {
+    let character_image = document.getElementById(`${character}`)
+    character_image.src = `images/${character}${choice}.png`
+}
+
+/**
+ * this function manage the display of the right text image depending on the result of the round.
+ * @param {string} result : the result of the round, to display
+ * @param {element} result_image : the image to be replaced
+ */
+function display_result_test(result_image, result) {
+    result_image.src = `images/${result}.png`
+}
+
+/**
+ * this function manage the end of the game.
+ * @param {string} looser : indicate which character as lost (to display the K.O.)
+ */
+function endgame(looser) {
+    let looserKOimage = document.getElementById(`KO${looser}`)
+    display_element(looserKOimage)
+    display_element(btnnewgame)
+    hide_element(vsicon)
+    hide_element(choicesection)
+    hide_element(result_image)
+}
